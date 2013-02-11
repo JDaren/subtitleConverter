0dacb861638ca50dacb20a3653ad91b0d1a02a86
JDaren <nemrioss-gh@yahoo.es>
2013-02-11 22:58:59 +0100

    simplification from interface

--- Subtitle Files/src/Pruebas.java
+++ Subtitle Files/src/Pruebas.java
@@ -32,28 +32,28 @@ public class Pruebas {
 			File file = new File("standards\\SRT\\Avengers.2012.Eng.Subs.srt");
 			InputStream is = new FileInputStream(file);
 			tto = ttff.parseFile(file.getName(), is);
-			IOClass.escribirFicheroTxt("prueba.txt", ((FormatSRT)ttff).toFile(tto));
+			IOClass.escribirFicheroTxt("prueba.txt", tto.toSRT());
 			
 			//To test the correct implementation of the ASS/SSA parser and writer.
 			ttff = new FormatASS();
 			file = new File("standards\\ASS\\test.ssa");
 			is = new FileInputStream(file);
 			tto = ttff.parseFile(file.getName(), is);
-			IOClass.escribirFicheroTxt("prueba.txt", ((FormatASS)ttff).toFile(tto));
+			IOClass.escribirFicheroTxt("prueba.txt", tto.toASS());
 
 			//To test the correct implementation of the TTML parser and writer.
 			ttff = new FormatTTML();
 			file = new File("standards\\XML\\Debate0_03-03-08.dfxp.xml");
 			is = new FileInputStream(file);
 			tto = ttff.parseFile(file.getName(), is);
-			IOClass.escribirFicheroTxt("prueba.txt", ((FormatTTML)ttff).toFile(tto));
+			IOClass.escribirFicheroTxt("prueba.txt", tto.toTTML());
 
 			//To test the correct implementation of the SCC parser and writer.
 			ttff = new FormatSCC();
 			file = new File("standards\\SCC\\sccTest.scc");
 			is = new FileInputStream(file);
 			tto = ttff.parseFile(file.getName(), is);
-			IOClass.escribirFicheroTxt("prueba.txt", ((FormatSCC)ttff).toFile(tto));
+			IOClass.escribirFicheroTxt("prueba.txt", tto.toSCC());
 			
 			//To test the correct implementation of the STL parser and writer.
 			ttff = new FormatSTL();
@@ -63,7 +63,7 @@ public class Pruebas {
 			OutputStream output = null;
 			try {
 				output = new BufferedOutputStream(new FileOutputStream("prueba.txt"));
-				output.write(((FormatSTL)ttff).toFile(tto));
+				output.write(tto.toSTL());
 			} finally {
 				output.close();
 			}
